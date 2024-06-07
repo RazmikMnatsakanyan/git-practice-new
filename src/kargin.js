c532edf8cc2b494f97fab17a77c7f9369e2067c2
Razmik Mnatsakanyan <razmik.mnatsakanyann@gmail.com>
2024-06-07 04:27:51 +0400

    Kargin commit 1

---
+++ src/kargin.js
@@ -0,0 +1,10 @@
+function getMeme(sketchNumber){
+  switch (sketchNumber){
+    case 1:
+      return "Patchary intelektna";
+    case 2:
+      return "Es pahanjum em vor da ani imenno harsy"
+    case 3:
+      return "Ba iranq inchi hamar en. Axji heleq"
+  }
+}
