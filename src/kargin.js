2aeaa4209eb7b2687a4dc637e371b29b42d11698
Razmik Mnatsakanyan <razmik.mnatsakanyann@gmail.com>
2024-06-07 04:49:23 +0400

    Kargin done

--- src/kargin.js
+++ src/kargin.js
@@ -7,6 +7,10 @@ function getMeme(sketchNumber){
     case 3:
       return "Ba iranq inchi hamar en. Axji heleq";
     case 4:
-      return "Podonok, skotina, otbros obshestva, chmotenas indz. Prosto shelkunchik"
+      return "Podonok, skotina, otbros obshestva, chmotenas indz. Prosto shelkunchik";
+    case 5:
+      return "Che, t@riq 40 metr en kom";
+    case 6:
+      return "De es helnum em txeqi het blot xaghalu";
   }
 }
